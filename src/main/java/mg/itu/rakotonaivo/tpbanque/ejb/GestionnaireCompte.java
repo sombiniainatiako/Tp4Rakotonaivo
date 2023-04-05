0690663ca8988c174a0273320c0f9430de89483a
RAKOTONAIVO RADO Tiako Sombiniaina <sombitiako@gmail.com>
2023-04-05 15:46:28 +0300

    Ajout EJB GestionnaireCompte

---
+++ src/main/java/mg/itu/rakotonaivo/tpbanque/ejb/GestionnaireCompte.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rakotonaivo.tpbanque.ejb;
+import jakarta.ejb.Stateless;
+
+@Stateless
+public class GestionnaireCompte {
+    
+}
