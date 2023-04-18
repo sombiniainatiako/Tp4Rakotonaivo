d20b89b6b69e3bfb0bdbdffe55b61737cb2f97d6
RAKOTONAIVO RADO Tiako Sombiniaina <sombitiako@gmail.com>
2023-04-18 11:21:46 +0300

    refactoring ListeComptes

--- src/main/java/mg/itu/rakotonaivo/tpbanque/jsf/ListeComptes.java
+++ src/main/java/mg/itu/rakotonaivo/tpbanque/jsf/ListeComptes.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
-package mg.itu.rakotonaivo.tpbanque.ejb;
+package mg.itu.rakotonaivo.tpbanque.jsf;
 
 import jakarta.ejb.EJB;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import java.io.Serializable;
 import java.util.List;
+import mg.itu.rakotonaivo.tpbanque.ejb.GestionnaireCompte;
 import mg.itu.rakotonaivo.tpbanque.ejb.GestionnaireCompte;
 import mg.itu.rakotonaivo.tpbanque.entities.CompteBancaire;
 import mg.itu.rakotonaivo.tpbanque.jsf.util.Util;
